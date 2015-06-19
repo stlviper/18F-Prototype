5fa5914d50dd2007282aae13a3175f19591d952c
Doug Kurucz <doug.kurucz@ogsystems.com>
2015-06-19 17:54:58 -0400

    fix search service mock test

--- server/test/services/searchServiceTest.js
+++ server/test/services/searchServiceTest.js
@@ -2,24 +2,23 @@ var should = require('should');
 var assert = require('assert');
 var request = require('supertest');
 
-describe('Services', function() {
-	var url = 'http://localhost:8080/';
+var server = require('../../main');
 
+describe('Services', function() {
 	before(function(done) {
 		done();
 	});
 
 	describe('Search service', function() {
 		it('***placeholder*** should return a basic set of results on basic query', function(done) {
-			request(url)
+			request(server)
 				.get('/search')
+				.set('Accept', 'application/json')
+				.expect('Content-Type', 'text/html; charset=utf-8')
+				.expect(404)
 				.end(function(err, res) {
-					if (err) {
-						throw err;
-					}
-					// this is should.js syntax, very clear
-					res.should.have.status(400);
-					res.body.should.equal(JSON.stringify({ 'results': [] }));
+					should.not.exist(err);
+					res.body.should.eql({});
 					done();
 				});
 		});
