bdb604b721d2eaef989f1b8e3c9ea6216bfcec6e
Doug Kurucz <doug.kurucz@ogsystems.com>
2015-06-19 14:07:38 -0400

    initial spa structure

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./main/routes/index');
+var users = require('./main/routes/users');
 
 var app = express();
 
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/search', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
