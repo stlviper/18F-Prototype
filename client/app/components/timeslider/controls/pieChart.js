4deec8cb4057b83160911186b1af21f00833ddec
Nolan Hager <jnhager@nhager-mac.local>
2015-06-23 17:58:35 -0500

    Renaming the timeline to time slider in order to fit to the needs of the project

---
+++ client/app/components/timeslider/controls/pieChart.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var leapYear = function (year) {
+  return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+};
+
+var convertDecimalDate = function (decimalDate) {
+  var year = parseInt(decimalDate);
+  var reminder = decimalDate - year;
+  var daysPerYear = leapYear(year) ? 366 : 365;
+  var miliseconds = reminder * daysPerYear * 24 * 60 * 60 * 1000;
+  var yearDate = new Date(year, 0, 1);
+  yearDate = new Date(yearDate.getTime() + miliseconds);
+  var outputFormat = d3.time.format('%Y%m%d');
+  return outputFormat(yearDate);
+};
+
+var gender = {'0': 'Unknown', '1': 'Male', '2': 'Female'};
+
+
+openfdaviz.directive("pieChart", ['$parse', function ($parse) {
+  return {
+    restrict: 'AE',
+    replace: true,
+    template: '<div id="dateSlider"></div>',
+    link: function (scope, element, attrs) {
+      d3.select('#dateSlider').call(d3.slider()
+          .axis(true)
+          .min(_settings.minDate)
+          .max(_settings.maxDate)
+          .value([_settings.minDate, _settings.maxDate])
+          .on("slideend", function (evt, value) {
+            var minDate = convertDecimalDate(value[0]);
+            var maxDate = convertDecimalDate(value[1]);
+            updatePieData(minDate, maxDate);
+          })
+      );
+    }
+  };
+}]);
