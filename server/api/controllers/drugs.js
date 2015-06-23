f3f59c5ec28e17cd9b8d5b730ce4e3424d052972
Paul Bierly <paul.bierly@ogsystems.com>
2015-06-23 13:04:40 -0500

    fixed up the search api a bit. Changed something. check Swagger doc.

--- server/api/controllers/drugs.js
+++ server/api/controllers/drugs.js
@@ -12,21 +12,17 @@ module.exports = {
 
 function eventRangeCount (req, res) {
   var fdaUrl = 'https://api.fda.gov/drug/event.json?search=receivedate:[' + req.swagger.params.start.value + '+TO+' + req.swagger.params.end.value + ']&count=' + req.swagger.params.field.value;
-  request.get(
-    fdaUrl,
-    {json: true},
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.json(body.results);
-      }
-    }
-  );
+  getDataFromFdaApi(fdaUrl, function(data){
+    res.json(data);
+  });
 }
 
 function eventSearch(req, res) {
+
   var limit = req.swagger.params.limit.value || 100;
-  var start = req.swagger.params.start.value || 0;
-  var fdaUrl = 'https://api.fda.gov/drug/event.json?search='+req.swagger.params.field.value+':"'+req.swagger.params.value.value+'"&limit='+limit+'&skip='+start;
+  var start = req.swagger.params.skip.value || 0;
+  var fdaUrl = 'https://api.fda.gov/drug/event.json?search='+req.swagger.params.query.value+'&limit='+limit+'&skip='+start;
+
   getDataFromFdaApi(fdaUrl, function(data){
     res.json(data);
   });
