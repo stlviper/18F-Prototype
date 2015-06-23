6cc2fcfb1263a9b8abaf42e9a275f6b6cf2c7cfb
Doug Kurucz <doug.kurucz@ogsystems.com>
2015-06-23 16:23:15 -0400

    move header and footer to components, fix homepage layout

--- client/app/app.js
+++ client/app/app.js
@@ -12,23 +12,23 @@ window.openfdaviz.config(function ($stateProvider, $urlRouterProvider) {
       url: '/',
       views: {
         'nav': {
-          templateUrl: '/app/shared/nav/homepage.html'
+          templateUrl: '/app/components/nav/homepage.html'
         },
         'content': {
           templateUrl: '/app/partial-homepage.html',
           controller: 'HomeController'
         },
         'footer': {
-          templateUrl: '/app/shared/footer/homepage.html'
+          templateUrl: '/app/components/footer/homepage.html'
         }
       }
     })
     .state('search', {
       url: '/search',
       views: {
-        'nav': {templateUrl: '/app/shared/nav/standard.html'},
+        'nav': {templateUrl: '/app/components/nav/standard.html'},
         'content': {templateUrl: '/app/shared/search/search.html'},
-        'footer': {templateUrl: '/app/shared/footer/standard.html'}
+        'footer': {templateUrl: '/app/components/footer/standard.html'}
       }
     });
 
--- client/app/shared/layout/layoutController.js
+++ client/app/shared/layout/layoutController.js
@@ -2,8 +2,22 @@
 
 var config = {};
 openfdaviz.controller('LayoutController', ['$scope', '$http', function($scope, $http){
+  $scope.imagePath = '';
   angular.element(document).ready(function () {
-
+    if(!config){
+      $http.get('/config.json').success(function(resp){
+        config = resp;
+        setImagePath();
+      });
+    }
+    else{
+      setImagePath();
+    }
   });
+
+  function setImagePath(){
+    $scope.imagePath = config.paths.images;
+    $('#splash-page-background ').css('background', 'url(' + $scope.imagePath + 'splash.jpg) no-repeat center center fixed')
+  }
 }]);
 
