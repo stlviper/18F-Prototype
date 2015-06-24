42fe71e245a3f79052146bfc2a85e61dee35d5c6
Paul Bierly <paul.bierly@ogsystems.com>
2015-06-24 09:29:12 -0500

    tests for the API added

--- server/api/controllers/drugs.js
+++ server/api/controllers/drugs.js
@@ -6,36 +6,55 @@ var request = require('request');
 
 
 module.exports = {
-      rangecount: eventRangeCount,
-      search: eventSearch
+
+  eventSearch: function (req, res) {
+    getEventSearchData(req, function(data){
+      res.json(data);
+    });
+  },
+
+  eventRangeCount: function (req, res) {
+    getRangeCountData(req, function(data){
+      res.json(data);
+    });
+  },
+
+  tests: {
+    getRangeCountData: getRangeCountData,
+    getEventSearchData: getEventSearchData
+  }
+
 }
 
-function eventRangeCount (req, res) {
+
+function getRangeCountData(req, callback) {
   var fdaUrl = 'https://api.fda.gov/drug/event.json?search=receivedate:[' + req.swagger.params.start.value + '+TO+' + req.swagger.params.end.value + ']&count=' + req.swagger.params.field.value;
-  getDataFromFdaApi(fdaUrl, function(data){
-    res.json(data);
+  getDataFromFdaApi(fdaUrl, function (data) {
+    callback(data);
   });
 }
 
-function eventSearch(req, res) {
-
+function getEventSearchData(req, callback) {
   var limit = req.swagger.params.limit.value || 100;
   var start = req.swagger.params.skip.value || 0;
-  var fdaUrl = 'https://api.fda.gov/drug/event.json?search='+req.swagger.params.query.value+'&limit='+limit+'&skip='+start;
+  var fdaUrl = 'https://api.fda.gov/drug/event.json?search=' + req.swagger.params.query.value + '&limit=' + limit + '&skip=' + start;
 
-  getDataFromFdaApi(fdaUrl, function(data){
-    res.json(data);
+  getDataFromFdaApi(fdaUrl, function (data) {
+    callback(data);
   });
 }
 
 
 function getDataFromFdaApi(fdaUrl, callback) {
-  request.get(
-    fdaUrl,
-    {json: true},
+  request.get({
+    url: fdaUrl,
+    json: true
+    },
     function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        callback(body.results);
+      if (!error && (response.statusCode == 200 || 404)) {
+        callback(response.statusCode == 404 ? body : body.results);
+      } else {
+        callback({error: 'Request Failed'});
       }
     }
   );
--- server/test/api/controllers/drugs_test.js
+++ server/test/api/controllers/drugs_test.js
@@ -1,5 +1,5 @@
-var chai = require('chai').expect;
-
+var expect = require('chai').expect;
+var drugs = require('../../../api/controllers/drugs');
 
 describe('Testing Drugs Controller.', function(){
 
@@ -15,15 +15,73 @@ describe('Testing Drugs Controller.', function(){
 
   // This is primarily just a test endpoint and may end up being removed
   describe('Drugs eventRangeCount endpoint:', function() {
-    describe('Should return data in range of 2010/05/21 to 2015/04/10', function(){
-      it("Should return ", function () {
-        var mockReq = this.mockReq.swagger.params;
-        mockReq.start = {value: "20100521"};
-        mockReq.end = {value: "20150410"};
-        mockReq.field = {value: 'patient.patientsex'};
-        console.log(this.mockReq);
+    describe('Should return counts for each term in range of 2010/05/21 to 2015/04/10', function(){
+      it("Contains a value for at least one count", function (done) {
+        var mockReq = this.mockReq;
+        mockReq.swagger.params.start = {value: "20100521"};
+        mockReq.swagger.params.end = {value: "20150410"};
+        mockReq.swagger.params.field = {value: 'patient.patientsex'};
+
+        drugs.tests.getRangeCountData(mockReq, function(data){
+          expect(data[0]).to.exist;
+          expect(data[0].count).to.exist;
+          done();
+        });
+
 
       });
+    });
+    describe('Should return error object due to invalid field name', function(){
+      it("Contains an error object", function (done) {
+        var mockReq = this.mockReq;
+        mockReq.swagger.params.start = {value: "20100521"};
+        mockReq.swagger.params.end = {value: "20150410"};
+        mockReq.swagger.params.field = {value: 'patient.patien2tsex'};
+
+        drugs.tests.getRangeCountData(mockReq, function(data){
+          expect(data.error).to.exist;
+          done();
+        });
+
+      });
+    });
+  });
+
+  // Returns data by searching a field
+  describe('Should return array of search result objects with field set as value', function(){
+    it("Contains 11 valid result entries", function (done) {
+      var mockReq = this.mockReq;
+      mockReq.swagger.params.query = {value: "primarysourcecountry:us"};
+      mockReq.swagger.params.limit = {value: "11"};
+      mockReq.swagger.params.skip = {value: '20'};
+
+      drugs.tests.getEventSearchData(mockReq, function(data){
+        expect(data.length).to.equal(11);
+        expect(data[0]['@epoch']).to.exist;
+        expect(data[10]['@epoch']).to.exist;
+        var totalCorrectResults = 0;
+        for (var i = 0; i < data.length; i++){
+          if (data[i].primarysourcecountry.toLowerCase() == 'us') {
+            totalCorrectResults++;
+          }
+        }
+        expect(totalCorrectResults).to.equal(11);
+        done();
+      });
+    });
+  });
+  describe('Should return error object due to invalid field name', function(){
+    it("Contains an error object", function (done) {
+      var mockReq = this.mockReq;
+      mockReq.swagger.params.query = {value: "primarysource23country:us"};
+      mockReq.swagger.params.limit = {value: "11"};
+      mockReq.swagger.params.skip = {value: '20'};
+
+      drugs.tests.getEventSearchData(mockReq, function(data){
+        expect(data.error).to.exist;
+        done();
+      });
+
     });
   });
 
