ac04415edc48ef4dc7c08feed0827819ba04fc0b
Paul Bierly <paul.bierly@ogsystems.com>
2015-06-24 04:31:32 -0500

    last commit didn't fix the broken tests. Fixed now

--- server/test/api/controllers/drugs_test.js
+++ server/test/api/controllers/drugs_test.js
@@ -1,18 +1,31 @@
 var chai = require('chai').expect;
 
-// This is primarily just a test endpoint and may end up being removed
-describe('Drugs eventRangeCount endpoint:', function() {
-  describe('Should return data in range of 2010/05/21 to 2015/04/10', function(){
-    it("Should return ", function () {
 
-      var mockReq = {swagger: {
-        params: {
-          start: {
-            value: "20100521"
-          },
+describe('Testing Drugs Controller.', function(){
 
-      }}};
+  beforeEach("Setup mock req/res objects", function(){
+    this.mockReq = {
+      swagger: {
+        params: {}
+      }
+    };
+    this.mockRes = {};
 
+  });
+
+  // This is primarily just a test endpoint and may end up being removed
+  describe('Drugs eventRangeCount endpoint:', function() {
+    describe('Should return data in range of 2010/05/21 to 2015/04/10', function(){
+      it("Should return ", function () {
+        var mockReq = this.mockReq.swagger.params;
+        mockReq.start = {value: "20100521"};
+        mockReq.end = {value: "20150410"};
+        mockReq.field = {value: 'patient.patientsex'};
+        console.log(this.mockReq);
+
+      });
     });
   });
+
+
 });
--- server/test/test.js
+++ server/test/test.js
@@ -1,2 +1,2 @@
 require('./api/helpers/geocoder_test.js');
-require('./api/controllers/drugs.js');
+require('./api/controllers/drugs_test.js');
