e4aa5535cc338f43f8155b159b443d355518cb61
Nolan Hager <jnhager@nhager-mac.local>
2015-06-24 14:37:45 -0500

    The results tab is hooked up to the search controller now

--- client/app/shared/search/searchController.js
+++ client/app/shared/search/searchController.js
@@ -1,16 +1,24 @@
 'use strict';
 
-openfdaviz.controller('SearchController', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
+openfdaviz.controller('SearchController', ['$scope', '$http', '$stateParams', function ($scope, $http, $stateParams) {
+  var _settings = {
+    generalApiCall: ''
+  };
   $scope.activeTab = 'query';
+  $scope.results = {
+    drugs: [{message: 'test'}, {message: 'test two'}],
+    devices: [{message: 'test three'}, {message: 'test four'}],
+    foods: [{message: 'test five'}, {message: 'test six'}]
+  };
 
-  if($stateParams.searchQuery){
-    console.log('test: '+$stateParams.searchQuery);
+  if ($stateParams.searchQuery) {
+    console.log('test: ' + $stateParams.searchQuery);
   }
 
   angular.element(document).ready(function () {
   });
 
-  $scope.activateSearchTab = function(activeTab){
+  $scope.activateSearchTab = function (activeTab) {
     $scope.activeTab = activeTab;
   }
 }]);
