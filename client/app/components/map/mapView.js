1e5007a2af605be2ce8b7ca6402e8cee34ae5d83
Luke Hogrebe <luke.hogrebe@ogsystems.com>
2015-06-24 09:22:36 -0500

    First fix attempt of the heatmap display when changing the zoom of the map. A nonlinear mapping function may be necessary based on the current zoom level.

--- client/app/components/map/mapView.js
+++ client/app/components/map/mapView.js
@@ -6,8 +6,7 @@ openfdaviz.directive('openfdavizMap', function(){
     templateUrl: '/app/components/map/mapView.html',
     link: function (scope, element, attrs) {
       // create a map in the "map" div, set the view to a given place and zoom
-      var map = L.map('map').setView([51.505, -0.09], 13);
-      map.panTo(new L.LatLng(38.8750, -77.4025));
+      var map = L.map('map', {center: [38, 264], zoom: 4, minZoom: 2});
 
       L.Icon.Default.imagePath = scope.imagePath;
 
@@ -17,16 +16,22 @@ openfdaviz.directive('openfdavizMap', function(){
       }).addTo(map);
 
       // add a marker in the given location, attach some popup content to it and open the popup
-      L.marker([51.5, -0.09]).addTo(map)
+      L.marker([38.4623, 267.69799]).addTo(map)
         .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
         .openPopup();
 
       // testing the heatmap
       var heatmap = new L.TileLayer.HeatCanvas("Heat Canvas", map, {},
-        {step:0.3, degree:HeatCanvas.QUAD, opacity:0.7});
-      heatmap.pushData(51.5, -0.09, 100);
-      heatmap.pushData(51.49, -0.09, 100);
+        {step:0.8, degree:HeatCanvas.QUAD, opacity:0.7});
+      heatmap.pushData(38.4623, 267.69799, map.getZoom()*4);
+      heatmap.pushData(38.3, 267.69799, map.getZoom()*4);
       map.addLayer(heatmap);
+
+      map.on('zoomend', function() {
+        heatmap.clear();
+        heatmap.pushData(38.4623, 267.69799, map.getZoom()*4);
+        heatmap.pushData(38.3, 267.69799, map.getZoom()*4);
+      });
     }
   }
 });
