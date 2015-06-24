76a8aa56b29c9d3e7d38561d1dae85d448ffa68d
Nolan Hager <jnhager@nhager-mac.local>
2015-06-24 10:51:07 -0500

    adding the search bar to the home page

--- client/app/components/searchbar/controls/searchbar.js
+++ client/app/components/searchbar/controls/searchbar.js
@@ -8,7 +8,6 @@ openfdaviz.directive('openfdavizSearchBar', function () {
       <div class="form-group has-feedback">\
         <input type="text" class="form-control" name="name" id="fdaSearch" placeholder="Search, Food, Drug, or Devices" value="" autocomplete="off">\
         <ul class="typeahead dropdown-menu"></ul>\
-        <button class="form-control-feedback" style="border:none;background:none;pointer-events:visible;" type="submit"><img src="/build/img/mag-glass.png" alt="Search"></button>\
         <a href="#/search" class="btn btn-lg btn-default">Submit</a>\
        </div>\
     </div>'
