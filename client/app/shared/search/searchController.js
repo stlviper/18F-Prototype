6636a44788292c9cd4a8b483df5f371426b97a49
Doug Kurucz <doug.kurucz@ogsystems.com>
2015-06-24 12:36:57 -0400

    add tab switching between query and results

--- client/app/shared/search/searchController.js
+++ client/app/shared/search/searchController.js
@@ -1,8 +1,13 @@
 'use strict';
 
 openfdaviz.controller('SearchController', ['$scope', '$http', function($scope, $http){
+  $scope.activeTab = 'query';
+
   angular.element(document).ready(function () {
     console.log('These are not the droids you are looking for.')
   });
-}]);
 
+  $scope.activateSearchTab = function(activeTab){
+    $scope.activeTab = activeTab;
+  }
+}]);
