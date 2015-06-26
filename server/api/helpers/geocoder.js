4f1eb67f73a98ab062252bcbd2818bb720e0e41b
Nolan Hager <jnhager@nhager-mac.local>
2015-06-26 07:14:15 -0500

    Back filling some api end point tests

--- server/api/helpers/geocoder.js
+++ server/api/helpers/geocoder.js
@@ -1,44 +1,59 @@
-fs = require('fs');
+var fs = require('fs'),
+  geocoderProvider = 'google',
+  httpAdapter = 'http',
+  geocoder = require('node-geocoder')(geocoderProvider, httpAdapter, {});
+
 
 global.geocodelist = null;
 
 function loadGeocoder() {
-    console.log("Loading Geocoder");
-    var res = fs.readFileSync('./api/helpers/countries_geocoded.json');
-    global.geocodelist = JSON.parse(res).data;
-    return global.geocodelist;
+  console.log("Loading Geocoder");
+  var res = fs.readFileSync('./api/helpers/countries_geocoded.json');
+  global.geocodelist = JSON.parse(res).data;
+  return global.geocodelist;
 }
 
 module.exports = {
 
-    geoCodeByISO3: function(ccc) {
-        if(ccc.length !== 3 ) {
-            return "Country code must only be three characters long";
-        }
-
-        if(global.geocodelist == null) {
-            loadGeocoder();
-        }
-        for(var i = 0; i < global.geocodelist.length; i++) {
-            if(global.geocodelist[i][10] === ccc.toUpperCase()) {
-                return global.geocodelist[i];
-            }
-        }
-    },
-
-    geoCodeByISO2: function (cc) {
-        if(cc.length !== 2) {
-            return "Country code must only be two characters long";
-        }
-
-        if(global.geocodelist == null) {
-            loadGeocoder();
-        }
-        for(var i = 0; i < global.geocodelist.length; i++) {
-            if(global.geocodelist[i][9] === cc.toUpperCase()) {
-                return global.geocodelist[i];
-            }
-        }
+  geoCodeByISO3: function (ccc) {
+    if (ccc.length !== 3) {
+      return "Country code must only be three characters long";
     }
+
+    if (global.geocodelist == null) {
+      loadGeocoder();
+    }
+    for (var i = 0; i < global.geocodelist.length; i++) {
+      if (global.geocodelist[i][10] === ccc.toUpperCase()) {
+        return global.geocodelist[i];
+      }
+    }
+  },
+
+  geoCodeByISO2: function (cc) {
+    if (cc.length !== 2) {
+      return "Country code must only be two characters long";
+    }
+
+    if (global.geocodelist == null) {
+      loadGeocoder();
+    }
+    for (var i = 0; i < global.geocodelist.length; i++) {
+      if (global.geocodelist[i][9] === cc.toUpperCase()) {
+        return global.geocodelist[i];
+      }
+    }
+  },
+
+  geoCodeString: function(information, callback){
+    /*
+     * NOTE: Information can be a string or an Array of strings for batch geocoding.
+     * for more information see:  https://github.com/nchaulet/node-geocoder
+     */
+    geocoder.geocode(information, function(err, res) {
+      callback(err, res);
+    });
+
+  }
 };
 
--- server/test/api/controllers/fda_api_test.js
+++ server/test/api/controllers/fda_api_test.js
@@ -109,7 +109,7 @@ describe('Testing Drugs Controller.', function () {
 
 
   describe('Should return an array of search results for Food events', function () {
-    it('Should', function (done) {
+    it('Contain an array of objects reflecting a group of food enforcement reports', function (done) {
       var mockReq = this.mockReq;
       mockReq.swagger.params.query = {value: "state:mo"};
       mockReq.swagger.params.limit = {value: "5"};
@@ -131,14 +131,64 @@ describe('Testing Drugs Controller.', function () {
     });
   });
 
-   /*describe('Should return an array of counts for a Range of records in Food Events', function () {
-   });
+  describe('Should return an array of counts for a Range of records in Food Events', function () {
+    it('Contain an array of enforcement reports for a time range', function (done) {
+      var mockReq = this.mockReq;
+      mockReq.swagger.params.query = {value: "state:ca"};
+      mockReq.swagger.params.limit = {value: "5"};
+      mockReq.swagger.params.skip = {value: '0'};
 
-   describe('Should return an array of search results for Device events', function () {
+      drugs.tests.getAPIData('device', 'enforcement', mockReq, function (data) {
+        expect(data.length).to.equal(5);
+        expect(data[0]['@epoch']).to.exist;
+        expect(data[3]['@epoch']).to.exist;
+        var totalCorrectResults = 0;
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].state.toLowerCase() == 'ca') {
+            totalCorrectResults++;
+          }
+        }
+        expect(totalCorrectResults).to.equal(5);
+        done();
+      });
+    });
+  });
 
-   });
+  describe('Should return an array of search results for Device events', function () {
+    it('Contain an array of Device enforcement events', function (done) {
+      var mockReq = this.mockReq;
+      mockReq.swagger.params.query = {value: "state:mo"};
+      mockReq.swagger.params.limit = {value: "5"};
+      mockReq.swagger.params.skip = {value: '20'};
 
-   describe('Should return an array of counts for a Range of records in Device Events', function () {
-   });*/
+      drugs.tests.getAPIData('food', 'enforcement', mockReq, function (data) {
+        expect(data.length).to.equal(5);
+        expect(data[0]['@epoch']).to.exist;
+        expect(data[3]['@epoch']).to.exist;
+        var totalCorrectResults = 0;
+        for (var i = 0; i < data.length; i++) {
+          if (data[i].state.toLowerCase() == 'mo') {
+            totalCorrectResults++;
+          }
+        }
+        expect(totalCorrectResults).to.equal(5);
+        done();
+      });
+    });
+  });
+
+  describe('Should return an array of counts for a Range of records in Device Events', function () {
+    it('Contain an array of device enforcement events for a time range', function (done) {
+      var mockReq = this.mockReq;
+      mockReq.swagger.params.start = {value: "20100521"};
+      mockReq.swagger.params.end = {value: "20150410"};
+      mockReq.swagger.params.field = {value: 'state'};
+      drugs.tests.getAPIRangeData('device', 'enforcement', 'recall_initiation_date', mockReq, function (data) {
+        expect(data[0]).to.exist;
+        expect(data[0].count).to.exist;
+        done();
+      });
+    });
+  });
 
 });
