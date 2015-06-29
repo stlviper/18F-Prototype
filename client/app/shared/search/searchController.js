1475dd21a577ce41a1ba47ea918bcd37ff60889f
Luke Hogrebe <luke.hogrebe@ogsystems.com>
2015-06-29 07:59:08 -0500

    Added boolean to search result entries to indicate whether they should be displayed based on search criteria Started to add heatmap filtering based on dates from the timeslider/calendar picker; only year filters are in this commit

--- client/app/shared/search/searchController.js
+++ client/app/shared/search/searchController.js
@@ -40,11 +40,10 @@ openfdaviz.controller('SearchController', ['$scope', '$http', '$stateParams', "l
 
   };
 
-
   $scope.activeResultsTab = 'drugs';
 
-  var minYear = '';
-  var maxYear = '';
+  var startDate = new Date("December 31, 2004");
+  var endDate = new Date("December 31, 2014");
   $scope.query = $stateParams.query;
   $scope.queryInProgress = false;
 
@@ -62,8 +61,10 @@ openfdaviz.controller('SearchController', ['$scope', '$http', '$stateParams', "l
 
   angular.element(document).ready(function () {
     $scope.$on('dateChange', function (event, data) {
-      minYear = data.minYear;
-      maxYear = data.maxYear;
+      startDate = data.minDate;
+      endDate = data.maxDate;
+      _filterSearchResults();
+      _updateHeatmap();
     });
   });
 
@@ -71,7 +72,7 @@ openfdaviz.controller('SearchController', ['$scope', '$http', '$stateParams', "l
     $stateParams.query = $scope.query;
     $scope.queryInProgress = true;
     $scope.results = emptyResults;
-    $.when.apply($, [queryDrugs(), queryFoods(), queryDevices()]).done(updateHeatmap);
+    $.when.apply($, [queryDrugs(), queryFoods(), queryDevices()]).done([_filterSearchResults, _updateHeatmap]);
   };
 
   var generalQuery = function () {
@@ -91,7 +92,7 @@ openfdaviz.controller('SearchController', ['$scope', '$http', '$stateParams', "l
       });
     return deferred;
   };
-  $.when(generalQuery()).done(updateHeatmap);
+  $.when(generalQuery()).done([_filterSearchResults, _updateHeatmap]);
 
   function queryDrugs() {
     var deferred = $.Deferred();
@@ -140,38 +141,48 @@ openfdaviz.controller('SearchController', ['$scope', '$http', '$stateParams', "l
     return deferred;
   }
 
-  function updateHeatmap() {
+  function _updateHeatmap() {
     $scope.queryInProgress = false;
+    $scope.layers.overlays.heat.data = [];
     // Plot food geodata points
-    if (typeof $scope.results.foods != 'undefined') {
+    if (typeof $scope.results.foods !== 'undefined') {
       _addPointsToHeatmap($scope.results.foods);
     }
     // Plot drug geodata points
-    if (typeof $scope.results.drugs != 'undefined') {
+    if (typeof $scope.results.drugs !== 'undefined') {
       _addPointsToHeatmap($scope.results.drugs);
     }
+    $scope.layers.overlays.heat.doRefresh = true;
+    // This is what people currently recommend to get the map to update immediately
+    leafletData.getMap().then(function(map) {
+      map.invalidateSize();
+    });
   }
 
   function _addPointsToHeatmap(category) {
     var perturbRadius = 0.004;
     for (var i in category) {
-      if (typeof category[i].GeoLocation != 'undefined') {
-        if (typeof category[i].GeoLocation.lat != 'undefined' &&
-          typeof category[i].GeoLocation.lng != 'undefined') {
-          var lat = Number(category[i].GeoLocation.lat);
-          var lng = Number(category[i].GeoLocation.lng);
-          var latLng = [lat, lng];
-          // Since the heatmap doesn't change when there are duplicate points,
-          // perturb duplicate points a little so there is a visible difference
-          // in the heatmap
-          for (var j = 0; j < $scope.layers.overlays.heat.data.length; j++) {
-            if (latLng[0] === $scope.layers.overlays.heat.data[j][0] &&
-              latLng[1] === $scope.layers.overlays.heat.data[j][1]) {
-              latLng = _perturbPoints(latLng, perturbRadius);
-              break;
+      if (typeof category[i].isDisplayable !== 'undefined') {
+        if (typeof category[i].GeoLocation !== 'undefined') {
+          if (typeof category[i].GeoLocation.lat !== 'undefined' &&
+            typeof category[i].GeoLocation.lng !== 'undefined') {
+            var lat = Number(category[i].GeoLocation.lat);
+            var lng = Number(category[i].GeoLocation.lng);
+            var latLng = [lat, lng];
+            // Since the heatmap doesn't change when there are duplicate points,
+            // perturb duplicate points a little so there is a visible difference
+            // in the heatmap
+            for (var j = 0; j < $scope.layers.overlays.heat.data.length; j++) {
+              if (latLng[0] === $scope.layers.overlays.heat.data[j][0] &&
+                latLng[1] === $scope.layers.overlays.heat.data[j][1]) {
+                latLng = _perturbPoints(latLng, perturbRadius);
+                break;
+              }
+            }
+            if (category[i].isDisplayable) {
+              $scope.layers.overlays.heat.data.push(latLng);
             }
           }
-          $scope.layers.overlays.heat.data.push(latLng);
         }
       }
     }
@@ -184,4 +195,84 @@ openfdaviz.controller('SearchController', ['$scope', '$http', '$stateParams', "l
     latLng[1] += radius*Math.cos(angle);
     return latLng;
   }
+
+  function _filterSearchResults() {
+    if (typeof $scope.results.foods !== 'undefined') {
+      _filterFoodSearchResults($scope.results.foods);
+    }
+    if (typeof $scope.results.drugs !== 'undefined') {
+      _filterDrugSearchResults($scope.results.drugs);
+    }
+    if (typeof $scope.results.devices !== 'undefined') {
+      _filterDeviceSearchResults($scope.results.devices);
+    }
+  }
+
+  function _filterFoodSearchResults(foods) {
+    for (var i in foods) {
+      if (typeof foods[i].isDisplayable === 'undefined') {
+        foods[i].isDisplayable = true;
+      }
+      if (typeof foods[i].report_date !== 'undefined') {
+        if (!_isDateInBounds(foods[i].report_date)) {
+          foods[i].isDisplayable = false;
+        } else {
+          foods[i].isDisplayable = true;
+        }
+      }
+    }
+  }
+
+  function _filterDrugSearchResults(drugs) {
+    for (var i in drugs) {
+      if (typeof drugs[i].isDisplayable === 'undefined') {
+        drugs[i].isDisplayable = true;
+      }
+      if (typeof drugs[i].receivedate !== 'undefined') {
+        if (!_isDateInBounds(drugs[i].receivedate)) {
+          drugs[i].isDisplayable = false;
+        } else {
+          drugs[i].isDisplayable = true;
+        }
+      }
+    }
+  }
+
+  function _filterDeviceSearchResults(devices) {
+    for (var i in devices) {
+      if (typeof devices[i].isDisplayable === 'undefined') {
+        devices[i].isDisplayable = true;
+      }
+      if (typeof devices[i].date_of_event !== 'undefined') {
+        if (!_isDateInBounds(devices[i].date_of_event)) {
+          devices[i].isDisplayable = false;
+        } else {
+          devices[i].isDisplayable = false;
+        }
+      }
+    }
+  }
+
+  function _isDateInBounds(dateToCheckString) {
+
+    // Check year
+    var reYear = /((19|20)\d{2})/;
+    var dateToCheckYear = Number(dateToCheckString.match(reYear)[0]);
+
+    var startDateYear = Number(startDate.getFullYear().toString());
+    if (dateToCheckYear < startDateYear) {
+      return false;
+    }
+    var endDateYear = Number(endDate.getFullYear().toString());
+    if (dateToCheckYear > endDateYear) {
+      return false;
+    }
+
+    // Check month
+
+    // Check day
+
+    return true;
+  }
+
 }]);
