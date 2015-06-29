684241981ee040911c4d738376389fdc360a4d61
Doug Kurucz <doug.kurucz@ogsystems.com>
2015-06-29 18:18:03 -0400

    fixing case sensitivity issue, temporary rename

--- client/app/components/searchbar/controls/searchBar2.js
+++ client/app/components/searchbar/controls/searchBar2.js
@@ -4,7 +4,7 @@ openfdaviz.directive('openfdavizSearchBar', function () {
   return {
     restrict: 'AE',
     replace: 'true',
-    templateUrl: '/app/components/searchbar/controls/searchBar.html',
+    templateUrl: '/app/components/searchbar/controls/searchBar2.html',
     controller: 'SearchBarController'
   }
 });
